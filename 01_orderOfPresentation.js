6868c364d59edf80d9120d78d115284fa3b9fc3b
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-06-24 13:16:31 +0900

    1번 문제 테스트

--- 01_orderOfPresentation.js
+++ 01_orderOfPresentation.js
@@ -1,1 +1,9 @@
-// 1번 문제 테스트
+function orderOfPresentation(N, K) {
+    // TODO: 여기에 코드를 작성합니다.
+}
+
+let output = orderOfPresentation(3, [2, 3, 1]);
+console.log(output); // 3
+
+output = orderOfPresentation(5, [1, 3, 2, 4, 5])
+console.log(output); // 6
