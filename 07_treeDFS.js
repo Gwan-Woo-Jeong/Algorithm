9675649f8f37ea6a4d3bda7d542c8d7eb9f75185
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-07-05 22:41:18 +0900

    problem 07 finished

--- 07_treeDFS.js
+++ 07_treeDFS.js
@@ -24,6 +24,19 @@ let dfs = function (node) {
     return res;
 };
 
+// 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
+let Node = function (value) {
+    this.value = value;
+    this.children = [];
+};
+
+// 위 Node 객체로 구성되는 트리는 매우 단순한 형태의 트리입니다.
+// membership check(중복 확인)를 따로 하지 않습니다.
+Node.prototype.addChild = function (child) {
+    this.children.push(child);
+    return child;
+};
+
 //입출력 예시
 let root = new Node(1);
 let rootChild1 = root.addChild(new Node(2));
