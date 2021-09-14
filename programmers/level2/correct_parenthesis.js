4db5bde2abac05cd3666c4b15e1130ece6a55c7a
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-09-14 19:39:48 +0900

    programmers correct_parenthesis finished

---
+++ programmers/level2/correct_parenthesis.js
@@ -0,0 +1,49 @@
+/*
+올바른 괄호
+문제 설명
+괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+"()()" 또는 "(())()" 는 올바른 괄호입니다.
+")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+문자열 s의 길이 : 100,000 이하의 자연수
+문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+
+입출력 예
+s	answer
+"()()"	true
+"(())()"	true
+")()("	false
+"(()("	false
+
+입출력 예 설명
+입출력 예 #1,2,3,4
+문제의 예시와 같습니다.
+*/
+
+function solution(s) {
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(s[i]);
+        } else if (s[i] === ")") {
+            const top = stack.pop();
+            if (top !== "(") return false;
+        }
+    }
+    return stack.length === 0;
+}
+
+let output1 = solution("()()"); // -> true
+console.log(output1);
+
+let output2 = solution("(())()"); // -> true
+console.log(output2);
+
+let output3 = solution(")()("); // -> false
+console.log(output3);
+
+let output4 = solution("(()("); // -> false
+console.log(output4);
