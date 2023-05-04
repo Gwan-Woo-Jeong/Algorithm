9d2ac65927f2c07339df4a14e2b7d4c8d891393b
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-05-05 01:06:29 +0900

    baekjoon - 4179 불 (bfs) fin

--- baekjoon/4179/4179.js
+++ baekjoon/4179/4179.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const splitStr = "\r\n";
+const splitStr = process.platform !== "win32" ? "\n" : "\r\n";
 const input = fs
   .readFileSync(path.resolve(__dirname, filepath))
   .toString()
@@ -12,70 +12,81 @@ const [R, C] = input[0].split(" ").map(Number);
 const matrix = input.slice(1, R + 1).map((el) => el.split(""));
 
 function solution(R, C, matrix) {
-  // 미로에서 초기 위치(J)와 불이 난 공간 (F)를 찾아 BFS 실행
-  // BFS 함수 2가지
-  // fbfs 함수 : F 위치를 중심으로 거리 표시 (-1)
-  // jbfs 함수 : fbfs 리턴한 matrix에 J 위치를 중심으로 거리 표시. 음수 + 양수의 결과가 양수면 갈 수 있는 길, 음수면 못가는 길
-  // 좌표 탐색 시 ny, nx 위치가 미로의 끝자리 인덱스라면 최단거리 + 2 리턴
-  // F는 좌표에 F를 표시하며 미로를 탐색
-  // 만나지 못하면 IMPOSSIBLE 리턴
-
-  const fMatrix = matrix.slice();
-
+  // 불(F) 큐와 지훈(J) 큐
   const fQueue = [];
   const jQueue = [];
 
   for (let y = 0; y < R; y++) {
     for (let x = 0; x < C; x++) {
-      if (matrix[y][x] === "J") jQueue.push([y, x, 0]);
+      // 미로에 위치한 J와 F를 찾음
+      if (matrix[y][x] === "J") {
+        // J가 미로의 가장 자리에 있을 경우, 즉시 탈출
+        if (y === 0 || x === 0 || y === R - 1 || x === C - 1) return 1;
+        // J 큐에 좌표 추가
+        jQueue.push([y, x, 0]);
+      }
       if (matrix[y][x] === "F") {
+        // F 좌표 값을 0으로 초기화
         matrix[y][x] = 0;
+        // F 큐에 좌표 추가
         fQueue.push([y, x, 0]);
       }
     }
   }
 
+  // 좌표 탐색 방향
   const dx = [1, -1, 0, 0];
   const dy = [0, 0, 1, -1];
 
+  // F 큐가 빌 때 까지 (미로를 모두 탐색)
   while (fQueue.length) {
+    // F 큐에서 좌표 하나를 꺼냄
     const [y, x, cnt] = fQueue.shift();
 
-    for (let i = 0; i < dx.length; i++) {
+    for (let i = 0; i < 4; i++) {
+      // 좌표의 상하좌우 4 부분을 모두 탐색
       const [ny, nx] = [y + dy[i], x + dx[i]];
-
+      // 탐색 좌표가 유효 범위 안이고
       if (ny >= 0 && nx >= 0 && ny < R && nx < C) {
-        if (fMatrix[ny][nx] === "." || fMatrix[ny][nx] === "J") {
-          fMatrix[ny][nx] = cnt - 1;
-          fQueue.push([ny, nx, cnt - 1]);
+        // 탐색 좌표 값이 .이거나 J일 때,
+        if (matrix[ny][nx] === "." || matrix[ny][nx] === "J") {
+          // 탐색 좌표에 불이 번진 시간 표시
+          matrix[ny][nx] = cnt + 1;
+          // F 큐에 해당 좌표 추가
+          fQueue.push([ny, nx, cnt + 1]);
         }
       }
     }
   }
 
+  // J 큐가 빌 때 까지 (미로를 모두 탐색)
   while (jQueue.length) {
+    // J 큐에서 좌표 하나를 꺼냄
     const [y, x, cnt] = jQueue.shift();
+    // 맨 처음, 시간이 0일 때 현재 좌표 값 0으로 초기화
+    if (cnt === 0) matrix[y][x] = 0;
 
-    for (let i = 0; i < dx.length; i++) {
+    for (let i = 0; i < 4; i++) {
+      // 좌표의 상하좌우 4 부분을 모두 탐색
       const [ny, nx] = [y + dy[i], x + dx[i]];
-
+      // 탐색 좌표가 유효 범위 안이고
       if (ny >= 0 && nx >= 0 && ny < R && nx < C) {
-        if (fMatrix[ny][nx] === ".") {
-          if (ny === 0 || nx === 0 || ny === R - 1 || nx === C - 1)
-            return cnt + 2;
+        // 탐색 좌표 값이 .이거나 불이 번진 시간보다 지훈의 이동 시간이 빠를 때,
+        if (matrix[ny][nx] === "." || matrix[ny][nx] > cnt + 1) {
+          // 지훈의 이동 시간 표시
           matrix[ny][nx] = cnt + 1;
+          // J 큐에 해당 좌표 추가
           jQueue.push([ny, nx, cnt + 1]);
-        } else if (fMatrix[ny][nx] < 0 && fMatrix[ny][nx] + cnt + 1 < 0) {
+          // 이동할 수 있는 좌표가 미로의 가장 자리일 경우
           if (ny === 0 || nx === 0 || ny === R - 1 || nx === C - 1)
+            // 이동 시간 + 2를 리턴 (미로 바깥으로 이동하기 때문)
             return cnt + 2;
-          matrix[ny][nx] = cnt + 1;
-          jQueue.push([ny, nx, cnt + 1]);
         }
       }
     }
   }
-  console.log(matrix);
 
+  // 마지막까지 미로의 가장 자리에 도달하지 못할 경우 IMPOSSIBLE 리턴
   return "IMPOSSIBLE";
 }
 
